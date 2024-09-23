47bac36b68b8bd40c6e565ecfabad2eb9818b750
Will-Cupa <adjm.sandoz@outlook.fr>
2024-09-23 23:28:31 +0200

    finally it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 using namespace std;
 
-int main(){
-    cout << "hello\n";
+int main(int argc, char * argv[]){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Quit();
     return 0;
 }
+
+
+//to compile
+//g++ main.cpp -ID:\C++Libraries\SDL2\include -LD:\C++Libraries\SDL2\lib -lmingw32 -lSDL2main -lSDL2 -o main
