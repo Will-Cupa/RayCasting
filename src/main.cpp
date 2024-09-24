8542f83857eddc80b888f24898e263bbcdbae8e6
Will-Cupa <adjm.sandoz@outlook.fr>
2024-09-24 23:13:49 +0200

    file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-void drawMaze(SDL_Renderer *renderer, int wallSize);
+void drawMaze(SDL_Renderer *renderer, const string &maze, int wallSize, int x, int y);
+
+string readFile(ifstream &file);
 
 int main(int argc, char *argv[]){
     int init = SDL_Init(SDL_INIT_EVERYTHING);
-    if (init < 0){
+    if (init <  ){
         cerr << "SDL not initialized properly error code :" << init << endl;
         return init;
     };
 
-    SDL_Window *window = SDL_CreateWindow("Raycast", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Raycast", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 8  , 6  , SDL_WINDOW_SHOWN);
     
     if (window == nullptr){
         cerr << "Failed to create window" << endl;
@@ -30,6 +33,10 @@ int main(int argc, char *argv[]){
 
     SDL_Event events;
 
+    ifstream mazeFile("maze.txt");
+    string maze = readFile(mazeFile);
+    mazeFile.close();
+
     while(running){
         //Events input
         while (SDL_PollEvent(&events)){
@@ -43,13 +50,13 @@ int main(int argc, char *argv[]){
         //Update
 
         //Draw
-        SDL_Rect rectangle = {0, 0, 400, 200};
+        SDL_Rect rectangle = { ,  , 4  , 2  };
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer,  ,  ,  , 255);
         SDL_RenderClear(renderer); //not really clearing, more like filling
         
-        SDL_SetRenderDrawColor(renderer, 0,20,255,255);
-        drawMaze(renderer,40);
+        SDL_SetRenderDrawColor(renderer,  ,2 ,255,255);
+        drawMaze(renderer, maze, 4 , 3 , 5 );
         
         //SDL_RenderFillRect(renderer, &rectangle);
 
@@ -60,28 +67,39 @@ int main(int argc, char *argv[]){
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
+    return  ;
+}
+
+string readFile(ifstream &file){
+    string output, line;
+    output = "";
+
+    while(getline(file, line)){
+        output += "\n" + line;
+    };
+
+    return output;
 }
 
 
-void drawMaze(SDL_Renderer *renderer, int wallSize){
-    string maze = "XXXX\n000X\nXXXX\n";
+void drawMaze(SDL_Renderer *renderer, const string& maze, int wallSize, int pos_x, int pos_y){
+    //string maze = "XXXX\n   X\nXXXX\n";
     SDL_Rect wall;
-    int pos_x = 0;
-    int pos_y = 0;
-    int j = 0;
+    int x = pos_x;
+    int y = pos_y;
+    int j =  ;
 
-    for(int i = 0; i < maze.length(); i++){
+    for(int i =  ; i < maze.length(); i++){
         if(maze[i] == '\n'){
-            pos_y += wallSize;
-            pos_x = 0;
+            y += wallSize;
+            x = pos_x;
         } else {
-            if(maze[i] == 'X'){
-                wall = {pos_x, pos_y, wallSize, wallSize};
+            if(maze[i] == 'W'){
+                wall = {x, y, wallSize, wallSize};
                 SDL_RenderFillRect(renderer, &wall);
             } 
             
-            pos_x += wallSize;
+            x += wallSize;
         }   
     }
 
