81e54f67d0f3e641b1f6e14f0c06691ccb8ec74a
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2024-11-20 07:59:27 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]){
         }
 
         //Update
-        maze.getDistanceFromWall(player);
+        maze.getPlayerCell(player);
         //Check collision
         if(!maze.isColliding(player.getX() + input_x*PLAYER_SPEED, player.getY() + input_y*PLAYER_SPEED)){
             //Update position
